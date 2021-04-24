a009dc2ad4638b792bfc10b1eeb8c00f3b3081c1
student <kyn949@gmail.com>
2021-04-24 16:53:03 +0000

    First commit

---
+++ src/main/java/com/simplilearn/artifactsample/ArtifactSampleApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.artifactsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/artifactsample/ArtifactSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.artifactsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
